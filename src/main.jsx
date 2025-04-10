68dcb6b2fbdf62314441b27445fea446793c3cdd
AkashItlogiko <akashsahanwu089@gmail.com>
2025-04-10 18:02:00 +0600

    Done

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
