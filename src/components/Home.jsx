13fca3fa34f197765d80d4c72451f62510762195
AkashItlogiko <akashsahanwu089@gmail.com>
2025-04-12 17:34:05 +0600

    Done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Home = () => {
+  const [products, setProducts] = useState([]);
+  const [colors, setColors] = useState([]);
+  const [sizes, setSizes] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchAllProducts = async () => {
+      try {
+        const response = await axios.get('http://127.0.0.1:8000/api/products/');
+        console.log(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchAllProducts();
+  }, []);
   return <div>Home</div>;
 };
 
