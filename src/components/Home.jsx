cf0cfca4698d56da32a1c9b5e5f70619873383fd
AkashItlogiko <akashsahanwu089@gmail.com>
2025-04-15 13:26:35 +0600

    Done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,6 @@ const Home = () => {
   const [selectedColor, setSelectedColor] = useState('');
   const [selectedSize, setSelectedSize] = useState('');
   const [searchTerm, setSearchTerm] = useState('');
-  const [message, setMessage] = useState('');
 
   const handleColorSelectBox = e => {
     setSelectedSize('');
@@ -18,11 +17,10 @@ const Home = () => {
     setSelectedColor(e.target.value);
   };
   const handleSizeSelectBox = e => {
-    setSearchTerm('');
     setSelectedColor('');
+    setSearchTerm('');
     setSelectedSize(e.target.value);
   };
-
   const clearFilters = () => {
     setSelectedColor('');
     setSelectedSize('');
@@ -31,48 +29,87 @@ const Home = () => {
   useEffect(() => {
     const fetchAllProducts = async () => {
       try {
-        const response = await axiosRequest.get('/products/');
+        const response = await axiosRequest.get('products');
         setProducts(response.data.data);
-        setColors(response.data.colors);
+
+        // Extract unique colors
+        const uniqueColors = [];
+        response.data.data.forEach(product => {
+          product.colors.forEach(color => {
+            if (!uniqueColors.some(unique => unique.id === color.id)) {
+              uniqueColors.push(color);
+            }
+          });
+        });
+
+        setColors(uniqueColors);
         setSizes(response.data.sizes);
+        console.log('Unique Sizes:', response.data.data.sizes);
       } catch (error) {
         console.log(error);
       }
     };
     fetchAllProducts();
   }, []);
+
   return (
     <div className="row my-5">
       <div className="col-md-12">
         <div className="row">
           <div className="col-md-8 mx-auto">
             <div className="row">
-              <div className="col-md-4 md-2">
+              <div className="col-md-4">
                 <div className="mb-2">
-                  <span className="fw-bold">Filter by Color:</span>
+                  <label htmlFor="color_id" className="form-label fw-bold">
+                    Filter by Color:
+                  </label>
+                  <select
+                    name="color_id"
+                    id="color_id"
+                    value={selectedColor}
+                    onChange={e => handleColorSelectBox(e)}
+                    disabled={selectedSize || searchTerm}
+                    className="form-select"
+                  >
+                    <option value="" onClick={clearFilters}>
+                      All Colors
+                    </option>
+                    {colors.map(color => (
+                      <option value={color.id} key={color.id}>
+                        {color.name}
+                      </option>
+                    ))}
+                  </select>
                 </div>
-                <select
-                  name="color_id"
-                  id="color_id"
-                  defaultValue={selectedColor}
-                  onChange={e => handleColorSelectBox(e)}
-                  disabled={selectedSize || searchTerm}
-                  className="form-select"
-                ></select>
-                <option
-                  value=""
-                  disabled={!selectedColor}
-                  onChange={() => clearFilters()}
-                >
-                  All Colors
-                </option>
-                {colors?.map(color => (
-                  <option
-                    value={color.id} 
-                    key={color.id}>
-                    {color.name}
-                  </option>
-                ))}
+              </div>
+
+              <div className="col-md-4">
+                <div className="mb-2">
+                  <label htmlFor="size_id" className="form-label fw-bold">
+                    Filter by Size:
+                  </label>
+                  <select
+                    name="size_id"
+                    id="size_id"
+                    value={selectedSize}
+                    onChange={e => handleSizeSelectBox(e)}
+                    disabled={selectedColor || searchTerm}
+                    className="form-select"
+                  >
+                    <option
+                      value=""
+                      // disabled={!selectedSize}
+                      onClick={clearFilters}
+                    >
+                      All Sizes
+                    </option>
+                    {sizes?.map(size => (
+                      <option value={size.id} key={size.id}>
+                        {size.name}
+                      </option>
+                    ))}
+                  </select>
+                </div>
               </div>
             </div>
           </div>
