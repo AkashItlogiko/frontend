a23bdf41880f53f4060df1b0b7e6976d2bdf7fc7
AkashItlogiko <akashsahanwu089@gmail.com>
2025-04-17 02:00:29 +0600

    Done

--- src/components/products/ProductsList.jsx
+++ src/components/products/ProductsList.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ProductsListItem from './ProductListItem';
 
 const ProductsList = ({ products }) => {
-  return (
+  const[productsToShow,setProductsToShow] = useState(5);
+  const lodeMoreProducts=()=>{
+    if(productsToShow>products?.langth){
+      return;
+    }else{
+       setProductsToShow(prevProductsToShow=>prevProductsToShow+5);
+    }
+  }  
+
+    return (
     <div className="row my-5">
-      {products?.map(product => (
+      {products?.slice(0,productsToShow).map(product => (
         <ProductsListItem product={product} key={product.id} />
       ))}
+      {
+        productsToShow<products?.length &&  
+      
+        <div className='d-flex justify-content-center my-3'>
+              <button className="btn btn-sm btn-dark" onDoubleClick={()=>lodeMoreProducts()}>
+              
+                <i className='bi bi-arrow-clockwise'></i>{' '}
+                Load More
+              </button>
+        </div>
+      }
     </div>
   );
 };
