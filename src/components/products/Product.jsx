0230ded7ea4e367487f122652323661fca528a14
AkashItlogiko <akashsahanwu089@gmail.com>
2025-04-17 18:17:22 +0600

    Done

--- src/components/products/Product.jsx
+++ src/components/products/Product.jsx
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { axiosRequest } from '../../helpers/config';
 import Alert from '../layouts/Alert';
- 
+import Spinner from '../layouts/Spinner';
+import { Parser } from 'html-to-react';
 
 const Product = () => {
   const [product, setProducts] = useState([]);
@@ -31,12 +32,66 @@ const Product = () => {
     fetachProductBySlug();
   }, [slug]);
 
-  return <div className='card my-5'>
-    {
-      error ?
-        <Alert content={}
-     }
-  </div>;
+  return (
+    <div className="card my-5">
+      {error ? (
+        <Alert content={error} type="danger" />
+      ) : loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <div className="row g-0">
+            <div className="col-md-4 p-2">
+              <div>Product Image</div>
+            </div>
+            <div className="col-md-8">
+              <div className="card-dody">
+                <div className="d-flex justify-content-between">
+                  <h5 className="text-dark p-2">{product?.name}</h5>
+                  <h6 className="badge bg-danger m-2">${product?.price}</h6>
+                </div>
+              </div>
+              <div className="my-3">{Parser().parse(product?.desc)}</div>
+              <div className="d-flex justify-content-between">
+                <div className="d-flex justify-content-start align-items-center mb-3">
+                  {product.sizes?.map(size => (
+                    <span
+                      key={size.id}
+                      className="bg-light text-dark me-2 p-1 fw-bold"
+                    >
+                      <small>{size.name}</small>
+                    </span>
+                  ))}
+                </div>
+                <div className="me-2">
+                  {product.status == 1 ? (
+                    <span className="badge bg-success p-2">In Stock</span>
+                  ) : (
+                    <span className="badge bg-danger p-2">Out of Stock</span>
+                  )}
+                </div>
+              </div>
+              <div className="d-flex justify-content-start align-items-center mb-3">
+                {product.colors?.map(color => (
+                  <div
+                    key={color.id}
+                    className="border border-light-subtle border-2"
+                    style={{
+                      backgroundColor: color.name.toLowerCase(),
+                      height: '20px',
+                      width: '20px',
+                      borderRadius: '50%',
+                      marginRight: '5px',
+                    }}
+                  ></div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Product;
